9a076b86ca307ba6aa035f16b465f9580aba4995
Jayzee <joker_hjz@hotmail.com>
2021-01-28 19:27:17 +1300

    Create alipay.h

---
+++ src/ios/alipay.h
@@ -0,0 +1,5 @@
+#import "AppDelegate.h"
+
+@interface AppDelegate (CordovaAlipay)
+- (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+@end
