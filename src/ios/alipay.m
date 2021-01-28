b6e755998f9f6fc0e5d9f24751829aeaf69c5c64
Jayzee <joker_hjz@hotmail.com>
2021-01-28 23:51:52 +1300

    Update alipay.m

--- src/ios/alipay.m
+++ src/ios/alipay.m
@@ -9,12 +9,12 @@
 #pragma mark "API"
 - (void)pluginInitialize {
     CDVViewController *viewController = (CDVViewController *)self.viewController;
-    app_id = [viewController.settings objectForKey:@"alipayid"];
+    self.app_id = [viewController.settings objectForKey:@"alipayid"];
 }
 
 - (void)payment:(CDVInvokedUrlCommand*)command
 {
-    callbackId = command.callbackId;
+    self.callbackId = command.callbackId;
     NSString* orderString = [command.arguments objectAtIndex:0];
     NSString* appScheme = [NSString stringWithFormat:@"ali%@", app_id];
     [[AlipaySDK defaultService] payOrder:orderString fromScheme:appScheme callback:^(NSDictionary *resultDic) {
@@ -22,10 +22,10 @@
         
         if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
         } else {
             pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:resultDic];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+            [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
         }
         
     }];
@@ -42,10 +42,10 @@
             
             if ([[resultDic objectForKey:@"resultStatus"] isEqual: @"9000"]) {
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsDictionary:resultDic];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
             } else {
                 pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:resultDic];
-                [self.commandDelegate sendPluginResult:pluginResult callbackId:callbackId];
+                [self.commandDelegate sendPluginResult:pluginResult callbackId:self.callbackId];
             }
         }];
     }
