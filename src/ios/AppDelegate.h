ac72e168b0960e1256adb43eefa9f9745692698c
Jayzee <joker_hjz@hotmail.com>
2021-01-28 23:42:07 +1300

    Update AppDelegate.h

--- src/ios/AppDelegate.h
+++ src/ios/AppDelegate.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface APAppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
