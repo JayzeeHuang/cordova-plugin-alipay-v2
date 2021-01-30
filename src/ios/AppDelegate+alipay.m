80a9ac32992cdc018e26ab822f10024837ae9f6e
Jayzee <joker_hjz@hotmail.com>
2021-01-30 20:35:20 +1300

    Update AppDelegate+alipay.m

--- src/ios/AppDelegate+alipay.m
+++ src/ios/AppDelegate+alipay.m
@@ -5,17 +5,21 @@
 
 - (BOOL)application:(UIApplication *)app openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options
 {
-    if ([url.host isEqualToString:@"safepay"]) {
-        [[AlipaySDK defaultService] processOrderWithPaymentResult:url standbyCallback:^(NSDictionary *resultDic) {
-            CDVPluginResult *pluginResult;
-            
-            if ([[resultDic objectForKey:@"resultStatus"]  isEqual: @"9000"]) {
-                NSLog(@"alipay success");
-            } else {
-                NSLog(@"alipay error");
-            }
-        }];
+    if (!url) {
+        return NO;
     }
+    NSMutableDictionary * openURLData = [[NSMutableDictionary alloc] init];
+    [openURLData setValue:url forKey:@"url"];
+    if (options[UIApplicationOpenURLOptionsSourceApplicationKey]) {
+        [openURLData setValue:options[UIApplicationOpenURLOptionsSourceApplicationKey] forKey:@"sourceApplication"];
+    }
+    if (options[UIApplicationOpenURLOptionsAnnotationKey]) {
+        [openURLData setValue:options[UIApplicationOpenURLOptionsAnnotationKey] forKey:@"annotation"];
+    }
+    // all plugins will get the notification, and their handlers will be called
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLNotification object:url]];
+    [[NSNotificationCenter defaultCenter] postNotification:[NSNotification notificationWithName:CDVPluginHandleOpenURLWithAppSourceAndAnnotationNotification object:openURLData]];
+    return YES;
 }
 
 @end
